1197407e2b8b472729d0a59e0f489eaadfac9f28
jynzl <76736126+jynzl@users.noreply.github.com>
2022-04-18 22:17:17 +0300

    Update Unique_Hub_v2.lua

--- Unique_Hub_v2.lua
+++ Unique_Hub_v2.lua
@@ -1,5 +1,9 @@
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
 
+pcall(function()
+game.CoreGui:FindFirstChild("dosage's solaris gui").Parent = nil -- please ignore
+end)
+
 if game.PlaceId == 3829055572 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/jynzl/UniqueHub/main/lolz%20underground%20war"))()
 else
